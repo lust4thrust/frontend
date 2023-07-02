d8c53b3efeabfe4060d327f8c858cb8e843eb713
Goran Mekić <meka@tilda.center>
2023-07-03 01:13:51 +0200

    One cover per album

--- src/lib/landing/Albumi.svelte
+++ src/lib/landing/Albumi.svelte
@@ -3,22 +3,17 @@
     <h2 class="title">Albumi</h2>
     <div class="album">
       <h3>Da li si spremna za let?</h3>
-      <div class="formats">
-        <a
-          class="format"
-          href="/data/Lust%204%20Thrust%20-%20Da%20li%20si%20spremna%20za%20let%20(flac).zip"
-        >
-          <div class="omot" />
-          <div class="format-title">FLAC</div>
-        </a>
-        <a
-          class="format"
-          href="/data/Lust%204%20Thrust%20-%20Da%20li%20si%20spremna%20za%20let%20(ogg).zip"
-        >
-          <div class="omot" />
-          <div class="format-title">OGG</div>
-        </a>
-      </div>
+      <div class="omot" />
+      <a
+        href="/data/Lust%204%20Thrust%20-%20Da%20li%20si%20spremna%20za%20let%20(flac).zip"
+      >
+        FLAC
+      </a>
+      <a
+        href="/data/Lust%204%20Thrust%20-%20Da%20li%20si%20spremna%20za%20let%20(ogg).zip"
+      >
+        OGG
+      </a>
     </div>
   </div>
 </div>
@@ -50,26 +45,11 @@
     flex-direction: column;
   }
 
-  .formats {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    flex-direction: row;
-  }
-
-  .format {
-    margin: 10px;
-  }
-
   .omot {
     width: 200px;
     height: 200px;
     background-image: url('/img/da_li_si_spremna_za_let.png');
     background-size: 100% 100%;
     background-repeat: no-repeat;
-  }
-
-  .format-title {
-    text-align: center;
   }
 </style>
